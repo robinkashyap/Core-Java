a65169445ee5f5894138bba838d5a37dfa24c3ae
robinkashyap <rkashyapp21@gmail.com>
2024-10-06 00:41:38 +0530

    code added

---
+++ Variable.java
@@ -0,0 +1,10 @@
+class Variable 
+{
+    public static void main(String a[])
+    {
+        int num1 = 3;
+        int num2 = 8;
+        int result = num1 + num2;
+        System.out.println(result);
+    }
+}
