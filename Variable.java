bd640b7d281f2cdcb9d7e70de7d763dac2e40bcf
robinkashyap <rkashyapp21@gmail.com>
2024-10-06 01:15:46 +0530

    more variables added

--- Variable.java
+++ Variable.java
@@ -2,9 +2,27 @@ class Variable
 {
     public static void main(String a[])
     {
+        byte b = 127;
+        short s = 558;
+        long l = 5854l;
+
+        float f = 5.6f;
+        double d = 5.6;
+
+        char c = 'c';
+
+        boolean bc = true;
+
         int num1 = 3;
         int num2 = 8;
         int result = num1 + num2;
         System.out.println(result);
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(bc);
     }
 }
